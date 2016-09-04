c1aff9e13fcfe87faf42bc99c22a0075ce511049
Ryan Kendall <ryantkendallt@gmail.com>
2016-09-04 12:05:49 +0100

    added session methods as helpers so that the view can access them

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :user_signed_in?, :current_user
+
 
   def user_signed_in?
     current_user.present?
