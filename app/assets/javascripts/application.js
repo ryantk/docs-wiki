96bb5d8b0769649da1614017d234514a5bbfa7da
Ryan Kendall <ryantkendallt@gmail.com>
2016-09-06 21:17:51 +0100

    made ux a bit nicer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,29 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+  $('textarea.editable').keydown(function(e) {
+    var keyCode = e.keyCode || e.which; 
+
+    if (keyCode == 9) { 
+      e.preventDefault();
+
+      // get current text
+      var value = this.value;
+      // get current selection start
+      var start = this.selectionStart;
+      // get current selection end
+      var end   = this.selectionEnd;
+
+      var numSpacesInTab = 2;
+
+      // set the value to be the existing value but with two spaces injected between selection range
+      this.value = value.substring(0, start) + ' '.repeat(numSpacesInTab) + value.substring(end);
+
+      // increment cursor position
+      this.selectionStart = this.selectionEnd = start + numSpacesInTab;
+    }
+  });
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
     markdown.render(text).html_safe
   end
 
+  def active_li(path)
+    content_tag :li, nil, class: current_page?(path) ? 'active' : '' do
+      yield if block_given?
+    end
+  end
+
+  def greeting(name)
+    greetings = I18n.t('greetings')
+    "#{greetings}, #{name}"
+  end
+
 end
